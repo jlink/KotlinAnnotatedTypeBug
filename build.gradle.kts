2c9c923edd59b33ddbd88d4c6c14e96732d6555b
Johannes Link <business@johanneslink.net>
2021-12-03 10:55:32 +0100

    Add bug revealing code

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+}
+
+plugins {
+    kotlin("jvm") version "1.6.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform {
+        include("bug/**/*Bug.class")
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf(
+			"-Xemit-jvm-type-annotations"
+		)
+        jvmTarget = "16"
+        javaParameters = true // This makes the bug appear
+    }
+}
+
+tasks.withType<Wrapper> {
+    gradleVersion = "7.3.1"
+}
